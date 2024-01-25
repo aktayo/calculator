c17bd802dc266109db2bcf3245e5033e82ff8806
aktayo <akintayo.odeniran@gmail.com>
2024-01-25 11:02:00 +0100

    udated

---
+++ index.js
@@ -0,0 +1,73 @@
+let dispValue = document.getElementById('info');
+function display(val){
+    // alert(val);
+  dispValue.value += val;
+
+}
+// let result = 0;
+// function bttnVal(){
+//     const bttnValue = document.innerText;
+//     document.getElementById("bttnValue");
+// }
+// let button = document.querySelector("value");
+// button.addEventListener(click, alert(yes))
+
+// function getNumber(){
+
+    
+
+// }
+// function displayNumber(num){
+    
+// }
+// function calculator(){
+    
+//     switch (sign){
+//     case '+':
+//         add();
+//         break;
+//     case '-':
+//         sub();
+//         break;
+//     case '*' :
+//         mul();
+//         break;
+//     case '/':
+//         div();
+//         break;
+//     case '=':
+//         ans();
+//         break;
+//     case 'C' :
+//         clr();
+//         break;
+//     }
+// }
+// function add(){
+//    while(bttnVal){
+//     result += bttnVal;
+//    } 
+// }
+// function sub(){
+//     while(bttnVal){
+//         result -= bttnVal;
+//        }
+
+    
+// }
+// function div(){
+//     while(bttnVal){
+//         result /= bttnVal;
+//        }
+// }
+// function mul(){
+//     while(bttnVal){
+//         result *= bttnVal;
+//        }
+// }
+// function ans(){
+//     displayNumber(result);
+// }
+// function clr(){
+    
+// }
