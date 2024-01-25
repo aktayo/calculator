7e9a7e827836dd93ec7b7aadc37f9604da5b15f0
aktayo <akintayo.odeniran@gmail.com>
2024-01-25 21:44:54 +0100

    new function

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-let display = document.querySelector('input');
-let buttons = document.querySelectorAll('.bttn');
-let ans = document.querySelector('.equal');
-let clr = document.querySelector('.clear');
+let display = document.querySelector("input");
+let buttons = document.querySelectorAll(".bttn");
+let ans = document.querySelector(".equal");
+let clr = document.querySelector(".clear");
 // let multiply = document.querySelector('.mult');
- 
-buttons.forEach(function(buttn){
-    buttn.addEventListener('click', function(e){
-        let value = e.target.dataset.val;
-    display.value +=value;
-    let num =display.value;
+const flag = Boolean(false);
+buttons.forEach(function (buttn) {
+    if (flag=='true'){
+        display.value = "";
     }
-    )
-})
-clr.addEventListener('click', ()=>display.value ='');
-function res(){
-    display.forEach(digit);
-    console.log(digit);
-}
-ans.addEventListener('click', function (e){
-   display.value = eval(display.value) ;
-})
+  buttn.addEventListener("click", function (e) {
+    let value = e.target.dataset.val;
+    display.value += value;
+    let num = display.value;
+  });
+});
+clr.addEventListener("click", () => (display.value = ""));
+
+ans.addEventListener("click", function (e) {
+    display.value = eval(display.value);
+    let flag = 'true';
+  });
