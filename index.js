5d85cd1074fee1ebbe25dfd9e45a9649cb73ba44
aktayo <akintayo.odeniran@gmail.com>
2024-01-25 15:16:58 +0100

    input updated

--- index.js
+++ index.js
@@ -1,73 +1,13 @@
-let dispValue = document.getElementById('info');
-function display(val){
-    // alert(val);
-  dispValue.value += val;
-
-}
-// let result = 0;
-// function bttnVal(){
-//     const bttnValue = document.innerText;
-//     document.getElementById("bttnValue");
-// }
-// let button = document.querySelector("value");
-// button.addEventListener(click, alert(yes))
-
-// function getNumber(){
-
-    
-
-// }
-// function displayNumber(num){
-    
-// }
-// function calculator(){
-    
-//     switch (sign){
-//     case '+':
-//         add();
-//         break;
-//     case '-':
-//         sub();
-//         break;
-//     case '*' :
-//         mul();
-//         break;
-//     case '/':
-//         div();
-//         break;
-//     case '=':
-//         ans();
-//         break;
-//     case 'C' :
-//         clr();
-//         break;
-//     }
-// }
-// function add(){
-//    while(bttnVal){
-//     result += bttnVal;
-//    } 
-// }
-// function sub(){
-//     while(bttnVal){
-//         result -= bttnVal;
-//        }
-
-    
-// }
-// function div(){
-//     while(bttnVal){
-//         result /= bttnVal;
-//        }
-// }
-// function mul(){
-//     while(bttnVal){
-//         result *= bttnVal;
-//        }
-// }
-// function ans(){
-//     displayNumber(result);
-// }
-// function clr(){
-    
-// }
+let display = document.querySelector('input');
+let buttons = document.querySelectorAll('button');
+let ans = document.querySelector('equal');
+let clr = document.querySelector('.clear');
+ 
+buttons.forEach(function(butto){
+    butto.addEventListener('click', function(e){
+        let value = e.target.dataset.val;
+    display.value +=value;
+    let num =display.value;
+    console.log(num);}
+    )
+})
