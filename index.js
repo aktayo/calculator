f589b19443d8919fee87ba63cb952e83f7e37842
aktayo <akintayo.odeniran@gmail.com>
2024-01-25 16:07:38 +0100

    working

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 let display = document.querySelector('input');
-let buttons = document.querySelectorAll('button');
-let ans = document.querySelector('equal');
+let buttons = document.querySelectorAll('.bttn');
+let ans = document.querySelector('.equal');
 let clr = document.querySelector('.clear');
+// let multiply = document.querySelector('.mult');
  
-buttons.forEach(function(butto){
-    butto.addEventListener('click', function(e){
+buttons.forEach(function(buttn){
+    buttn.addEventListener('click', function(e){
         let value = e.target.dataset.val;
     display.value +=value;
     let num =display.value;
-    console.log(num);}
+    }
     )
+})
+clr.addEventListener('click', ()=>display.value ='');
+function res(){
+    display.forEach(digit);
+    console.log(digit);
+}
+ans.addEventListener('click', function (e){
+   display.value = eval(display.value) ;
 })
