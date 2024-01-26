d47286b803b528ef02180d3318f03edf072e4239
aktayo <akintayo.odeniran@gmail.com>
2024-01-26 23:17:10 +0100

    perfect

--- index.js
+++ index.js
@@ -1,22 +1,82 @@
-let display = document.querySelector("input");
+let display = document.getElementById("disp");
+let display2 = document.getElementById("disp2");
 let buttons = document.querySelectorAll(".bttn");
 let ans = document.querySelector(".equal");
 let clr = document.querySelector(".clear");
-// let multiply = document.querySelector('.mult');
+let signs = ["+", "-", "/", "x"];
+let num1 = "";
+let num2 = "";
+let operator = "";
+let result = "";
+let scrn = (i = op = scrn1 = symbol = "");
 const flag = Boolean(false);
+function calculate() {
+  digit1 = parseInt(num1);
+  digit2 = parseInt(num2);
+  switch (operator) {
+    case "+":
+      result = digit1 + digit2;
+      break;
+    case "-":
+      result = digit1 - digit2;
+      break;
+    case "x":
+      result = digit1 * digit2;
+      break;
+    case "/":
+      result = digit1 / digit2;
+      break;
+  }
+  display2.value = result;
+  num1 = result;
+}
+
 buttons.forEach(function (buttn) {
-    if (flag=='true'){
-        display.value = "";
-    }
   buttn.addEventListener("click", function (e) {
     let value = e.target.dataset.val;
-    display.value += value;
-    let num = display.value;
+
+    for (let i of signs) {
+      if (value == i && num1 != "") {
+        display.value += value;
+        symbol = i;
+
+        op = "done";
+        break;
+      }
+    }
+
+    if (op == "done" && num1 != "") {
+      operator = symbol;
+      op = "";
+    } else if (operator == "") {
+      display.value += value;
+      num1 = display.value;
+    } else if (operator != "") {
+      num2 += value;
+      display.value += num2;
+    }
+    if (operator != "" && num1 != "" && num2 != "") {
+      calculate();
+      num2 = "";
+    }
   });
 });
-clr.addEventListener("click", () => (display.value = ""));
-
-ans.addEventListener("click", function (e) {
-    display.value = eval(display.value);
-    let flag = 'true';
-  });
+function clear() {
+  display.value = "";
+  display2.value = "";
+  operator = "";
+  result = "";
+  num1=num2='';
+}
+function cal() {
+  if (num2 == "") {
+    display.value = result;
+    display2.value = "";
+  } else {
+    calculate();
+    display.value = result;
+    display2.value = "";
+  }
+}
+clr.addEventListener("click", clear);
+ans.addEventListener("click", cal);
